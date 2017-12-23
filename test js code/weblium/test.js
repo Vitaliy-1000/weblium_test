644b0cf7e760c9cc7f9879ade242eb883efe6664
Vitaliy <8.ecgtiysq.8@gmail.com>
2017-12-23 13:49:51 +0200

    new commit

--- test js code/weblium/test.js
+++ test js code/weblium/test.js
@@ -1,27 +1,87 @@
+const { expect } = require('chai')
+
+const { client, element } = require('wd-interface')
+
+const Landing = require('./landing.selector')
+
+describe('Weblium base example', () => {
+  let browser = null
+  const baseURL = 'https://weblium.com/'
+
+  const pricing = element(Landing.pricing)
+  const about = element(Landing.about)
+
+  //selectors
+
+  before(async () => {
+    browser = client().chrome() // for dirrect connection to chromedriver client().chrome(true)
+    await browser.goTo(baseURL)
+  })
+
+  after(async () => {
+    await browser.closeBrowser()
+  })
+/*
+  it('assert base url', async () => {
+    const url = await browser.getUrl()
+    expect(url).to.eql(baseURL)
+  })
+
+  it('click pricing', async () => {
+    await pricing.waitForElement(2000)
+    await pricing.click()
+    const url = await browser.getUrl()
+    expect(url).to.contains('pricing')
+
+    const body = element('body')
+
+    const borderPricing = await body.getElements('.border')
+
+    expect(borderPricing.length).to.eql(5)
+  })
+
+  it('click about', async () => {
+    await about.click()
+
+    const url = await browser.getUrl()
+
+    expect(url).to.contains('about')
+    const body = element('body')
+    const aboutContainer = await body.getElements('.container')
+    expect(aboutContainer.length).to.eql(7)
+  })
+*/
 
 
-const { expect } = require('chai');
+  it('login to my account', async () => {
+    const login = element(Landing.login);
+    await login.click();
+    const url = await browser.getUrl();
+    expect(url).to.contains('login');
 
-const { client, element } = require('wd-interface');
+    const email = element(Landing.loginForm.email);
+    const password = element(Landing.loginForm.password);
+    const submitLogin = element(Landing.loginForm.submitLogin);
 
-describe('Google base example', () => {
- let browser = null;
- const baseURL = 'https://www.google.com/';
+    expect(await email.isDisplayed()).to.eql(true);
+    expect(await password.isDisplayed()).to.eql(true);
+    expect(await submitLogin.isDisplayed()).to.eql(true);
 
- //selectors
- 
+    await email.sendKeys('dereva@dereva');
+    await password.sendKeys('123123');
+    await submitLogin.click();
 
- before(async () => {
-   browser = client().chrome(); // for dirrect connection to chromedriver client().chrome(true)
-   await browser.goTo(baseURL);
- });
+    await browser.sleep(1500);
+    
+    expect(await browser.getUrl()).to.contains('myaccount/websites');
 
- after(async () => {
-   await browser.closeBrowser();
- });
+  });
 
- it('assert base url', async () => {
-   const url = await browser.getUrl();
-   expect(url).to.eql(baseURL);
- });
+  it('go to my profile', async () => {
+    const profile = element(Landing.myAccount.leftMenuProfile);
+
+    expect(await profile.isDisplayed()).to.eql(true);
+    await profile.click();
+    expect(await browser.getUrl()).to.contains('profile');
+  });
 });
